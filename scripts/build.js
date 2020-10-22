2bb64146fc3bea536a700d1bdcd6f123251f57e0
Reece <me@rdil.rocks>
2020-10-22 18:02:19 -0400

    Continue to migrate

--- scripts/build.js
+++ scripts/build.js
@@ -7,6 +7,7 @@ process.on("unhandledRejection", (err) => {
 const chalk = require("chalk")
 const fs = require("fs-extra")
 const sass = require("sass")
+const child_process = require("child_process")
 const rimraf = require("rimraf").sync
 
 function build() {
@@ -14,7 +15,9 @@ function build() {
     rimraf("build")
 
     console.log("[1/5] Copying public folder")
-    copyPublic()
+    fs.copySync("public", "build", {
+        dereference: true,
+    })
 
     console.log("[2/5] Compiling sass bundle")
     const out = sass.renderSync({
@@ -24,12 +27,16 @@ function build() {
 
     console.log("[3/5] Writing sass bundle")
     fs.writeFileSync("build/bundle.min.css", out.css)
-}
 
-function copyPublic() {
-    fs.copySync("public", "build", {
+    console.log("[4/5] Building cherry")
+    child_process.execSync("cd cherry && yarn build")
+
+    console.log("[5/5] Copying cherry files")
+    fs.copySync("cherry/build", "build/cherry", {
         dereference: true,
     })
+
+    console.log(chalk.greenBright("\nDone!"))
 }
 
 build()
