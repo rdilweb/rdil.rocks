805ca6ed87a8f295a5e551864e8cecc713c9a15b
Reece Dunham <me@rdil.rocks>
2020-12-25 20:52:01 +0000

    Officially start
    Signed-off-by: Reece Dunham <me@rdil.rocks>

---
+++ root/gatsby-config.js
@@ -0,0 +1,14 @@
+const plugins = [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-svgr"
+]
+
+const siteMetadata = {
+    siteUrl: "https://rdil.rocks/"
+}
+
+module.exports = {
+    plugins,
+    siteMetadata
+}
---
+++ root/src/pages/index.jsx
@@ -0,0 +1,140 @@
+import React from "react"
+
+import NavBar from "../components/NavBar"
+
+import "../css/base.css"
+
+const Home = () => {
+    return (
+        <>
+            <NavBar />
+            <main class="content">
+                <div class="title">
+                    <h1>Reece Dunham</h1>
+                    <p>
+                        Heya, I'm a coder, designer of websites, and open-source
+                        lover.
+                    </p>
+                </div>
+                <div class="columns">
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <h3>About</h3>
+                                <p>
+                                    I am a software developer and I maintain
+                                    many open source projects on GitHub. I have
+                                    been coding since 2014, and can say I have
+                                    proficient knowledge inmost of the common
+                                    programming languages such as Python, HTML,
+                                    and JS to name a few.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <h3>Experience</h3>
+                                <p>
+                                    I have taken a large amount of classes on
+                                    Python (my main language) and web design,
+                                    plus some on other languages such as Java,
+                                    React, and the basics of Docker. I run
+                                    several OSS projects that have helped me to
+                                    learn how to apply code to the real world.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card">
+                            <div class="card-content">
+                                <h3>Hobbies</h3>
+                                <p>
+                                    When I'm not working on software, I am most
+                                    likely walking my dog, enjoying tennis,
+                                    taking a hike in the local woods, playing a
+                                    video game or two, listening to Spotify, or
+                                    watching Netflix. Or sleeping - that's great
+                                    for gaining energy to code more.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <h2>Get in touch</h2>
+                    <p>
+                        Want to have a chat? My contact info is on the sidebar.
+                        I'm looking forward to meeting you!
+                    </p>
+
+                    <h2>Things I've made</h2>
+                    <ul>
+                        <li>
+                            <a href="https://area4.readthedocs.io/en/stable">
+                                area4
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://privacy.rdil.rocks">
+                                My Privacy Journal
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/rdilweb">
+                                A few web libraries
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://docs.rdil.rocks">
+                                A lot of other things, some are documented here
+                            </a>
+                        </li>
+                    </ul>
+
+                    <h2>Open-source projects I've worked on</h2>
+                    <ul>
+                        <li>
+                            <a href="https://github.com/PowerShell/PowerShell">
+                                PowerShell Core
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/PowerShell/PowerShell-Docker">
+                                PowerShell Docker Images (I'm an official
+                                curator!)
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://cirruslabs.org">Cirrus CI</a>
+                        </li>
+                        <li>
+                            <a href="https://zlanguage.github.io">
+                                The Z programming language
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://opensourcedesign.net">
+                                Open Source Design
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2>More about me</h2>
+                    <ul>
+                        <li>
+                            <a href="/pages/code-style">My code style</a>
+                        </li>
+                    </ul>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Home
